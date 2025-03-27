a327329aac811159deb172908b0a2278afc58969
sauravkalaskar <skalaska@asu.edu>
2025-03-27 02:46:46 -0700

    Remove unused files and components from Angular Workflow Builder

--- workflow-builder/src/app/models/connection.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Connection {
-  sourceId: string; // ID of the source node
-  targetId: string; // ID of the target node
-  // Additional properties can be added as needed
-}
--- workflow-builder/src/app/models/node.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Node {
-  id: string;
-  title: string;
-  type: 'Start' | 'Action1' | 'Action2' | 'End';
-  inputPort: boolean;
-  outputPort: boolean;
-}
